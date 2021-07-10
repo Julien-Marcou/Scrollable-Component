de4e38a366c6b794791280774bba34fe5487ddee
Julien Marcou <julien.marcou.dev@gmail.com>
2021-07-10 21:27:07 +0200

    Add missing typing for the HTML element tag <scrollable-component>

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,9 @@ export class ScrollableComponentElement extends HTMLElement {
 	public readonly viewport: HTMLElement;
 	public readonly content: HTMLElement;
 }
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'scrollable-component': ScrollableComponentElement;
+  }
+}
