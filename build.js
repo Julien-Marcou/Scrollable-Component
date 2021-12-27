b187ac6129d666a00a407de8a107d9f931d5a9fd
Julien Marcou <julien.marcou.dev@gmail.com>
2021-12-27 17:38:23 +0100

    Add build script

---
+++ build.js
@@ -0,0 +1,41 @@
+import { minify as minifyCSS } from 'csso';
+import { minify as minifyHTML } from 'html-minifier-terser';
+import { minify as minifyJS } from 'terser';
+import { readFileSync, writeFileSync } from 'fs';
+
+const cssInputFilename = 'src/index.css';
+const htmlInputFilename = 'src/index.html';
+const jsInputFilename = 'src/index.js';
+const outputFilename = 'index.js';
+const cssToken = '{{COMPONENT_CSS}}';
+const htmlToken = '{{COMPONENT_HTML}}';
+
+const cssOptions = {
+  restructure: false,
+};
+const htmlOptions = {
+  collapseWhitespace: true,
+};
+const jsOptions = {
+  mangle: {
+    toplevel: true,
+    properties: {
+      reserved: ['vertical', 'horizontal', 'connectedCallback', 'attributeChangedCallback', 'observedAttributes']
+    },
+  },
+};
+
+const minifiedCSS = minifyCSS(
+  readFileSync(cssInputFilename).toString(),
+  cssOptions,
+).css;
+const minifiedHTML = await minifyHTML(
+  readFileSync(htmlInputFilename).toString(),
+  htmlOptions,
+);
+const minifiedJS = await minifyJS(
+  readFileSync(jsInputFilename).toString().replace(cssToken, minifiedCSS).replace(htmlToken, minifiedHTML),
+  jsOptions,
+);
+
+writeFileSync(outputFilename, minifiedJS.code);
