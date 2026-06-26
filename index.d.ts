458515e3ec38ca1f36707c069d4b956efcb0bc98
Julien Marcou <julien.marcou.dev@gmail.com>
2026-06-26 18:56:46 +0200

    Fix incorrect type declarations in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 export class ScrollableComponentElement extends HTMLElement {}
 
 export function defineScrollableComponent(): void;
-export function isScrollableComponentDefined(): void;
+export function isScrollableComponentDefined(): boolean;
 export function whenScrollableComponentDefined(): Promise<CustomElementConstructor>;
 
 declare global {
